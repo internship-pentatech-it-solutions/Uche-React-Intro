54caee802671784a16830bb388a3f74b3eaaaebd
joel042 <uchennajoeleze@gmail.com>
2024-10-12 15:53:22 +0100

    .

--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
 
 function Shop() {
+  const [image, setImage] = useState("setImage");
+   
+ useEffect(() => {
+ fetch("https://randomfox.ca/floof").then((response) =>response.json())
+ .then((data) => {
+  console.log(data.image);
+  setImage(data.image);
+  
+ })
+
+ },[])
+
   return (
     <div>
       <h1 className="text-gray-800 text-3xl text-center mt-10">
-        This is the Shop page
+        <img src={image} alt="" />
       </h1>
     </div>
   );
